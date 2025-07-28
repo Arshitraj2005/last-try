43f558af117e2576483f3f6cebbe600ef01eee1a
Arshitraj2005 <nigachu789@gmail.com>
2025-07-28 20:36:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "8363849977:AAF00Fv0TkNoPG-F9ORpk2DloAVe3e2x94k"
+STREAM_KEY = "mbbh-5q15-4khd-q11h-4cds"
+VIDEO_URL = "https://youtube.com/playlist?list=PLsYsKMiqFvj-HAimvJr_HJzn20vLpdxvZ&si=AjzLJTW54GnbVU4S"  # Or Google Drive video link (with direct link)

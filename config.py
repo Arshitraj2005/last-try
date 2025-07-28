6a276a0581a4d592751b3dfbd6eb58ae05d11db1
Arshitraj2005 <nigachu789@gmail.com>
2025-07-28 21:05:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-BOT_TOKEN = "8363849977:AAF00Fv0TkNoPG-F9ORpk2DloAVe3e2x94k"
-STREAM_KEY = "mbbh-5q15-4khd-q11h-4cds"
-VIDEO_URL = "https://youtube.com/playlist?list=PLsYsKMiqFvj-HAimvJr_HJzn20vLpdxvZ&si=AjzLJTW54GnbVU4S"  # Or Google Drive video link (with direct link)
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+OWNER_ID = 5326642169
